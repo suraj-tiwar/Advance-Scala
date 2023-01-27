259bf95a3557ae5abb9347124a1469c6f8c06e04
suraj-tiwar <61197325+suraj-tiwar@users.noreply.github.com>
2023-01-27 18:40:28 +0530

    27-01-23

--- src/lectures/part5ts/StructuralTypes.scala
+++ src/lectures/part5ts/StructuralTypes.scala
@@ -2,8 +2,37 @@ package lectures.part5ts
 
 object StructuralTypes extends App{
 
-  type JavaCloseable = java.io.Closeable
+  type JavaCloseable = java.io.Closeable //
   class HipsterCloseable {
     def close(): Unit = println("yeah yeah I'm closing ")
+
+    def closeSilently(): Unit = println("Hippster closing the gates")
   }
+
+  type UnifiedCloseable = {
+    def close(): Unit
+  }// STRUCTURAL TYPE
+  def closeQuietly(unifiedCloseable: UnifiedCloseable): Unit = unifiedCloseable.close() //
+
+  closeQuietly(new JavaCloseable {
+    override def close(): Unit = ???
+  })
+  closeQuietly(new HipsterCloseable)
+
+  // Type REFINEMENTS
+  type AdvanceCloseable = JavaCloseable {
+    def closeSilently(): Unit // adding new method to java.io.closeable package
+  }
+
+  class AdvancedJavaCloseable extends JavaCloseable {
+    override def close() : Unit = println("Java closes")
+    def closeSilently(): Unit = println("Java closes silently")
+  }
+  def closeShh(advCloseable : AdvanceCloseable):Unit = advCloseable.closeSilently()
+
+  closeShh(new AdvancedJavaCloseable)
+  def altClose(closeable : { def close() : Unit}): Unit = closeable.close()
+
+
+
 }
